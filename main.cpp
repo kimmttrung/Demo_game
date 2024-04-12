77e6c8a2624f75ef0d1db37ef6d2de5833f2d0bc
Mai Trung <maitantrung23012005@gmail.com>
2024-04-12 23:43:11 +0700

    Xóa đi để sửa lại

--- main.cpp
+++ main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
-#include "Engine.h"
-#include "Timer.h"
 
 
 int main(int argc, char* argv[])
 {
-    Engine::GetInstance()->Init();
-    while(Engine::GetInstance()->Running()){
-        Engine::GetInstance()->Events();
-        Engine::GetInstance()->Update();
-        Engine::GetInstance()->Render();
-        Timer::GetInstance()->Tick();
-    }
-
-    Engine::GetInstance()->Clean();
-
     return 0;
 }
-
-
